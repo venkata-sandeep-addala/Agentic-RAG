56f92b46884a913ba2abd56625b0b11ca4974f15
venkata-sandeep-addala <168465210+venkata-sandeep-addala@users.noreply.github.com>
2026-05-04 22:40:43 +0530

    complete which includes CRAG, Self-RAG & Adaptive RAG

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from graph.graph import graph
 
 if __name__ == "__main__":
     print("Hello Advanced RAG")
-    print(graph.invoke(input={"question": "IPL 2026?"})['generation'])
+    print(graph.invoke(input={"question": "How to make pizza?"})['generation'])
